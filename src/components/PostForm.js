0cdc1f8119681afdf37995bf444a6d72c62be470
sudeakq <sudeakq@gmail.com>
2023-08-22 11:54:20 +0300

    add submit

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,14 +1,46 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import Axios from 'axios'
 
 export default function PostForm() {
+    const url = " 
+    // your url here  
+    "
+    const [data, setData] = useState({
+        name: "",
+        date: "",
+        iduser: ""
+    })
+
+    function submit(e) {
+        e.preventDefault();
+        Axios.post(url, {
+            name: data.name,
+            data: data.date,
+            iduser: parseInt(data.iduser)
+
+        })
+            .then(res => {
+                console.log(res.data)
+            })
+
+    }
+
+
+    function handle(e) {
+        const newdata = { ...data }
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+    }
+
     return (
         <div>
-            <form>
-                <input placeholder="name" type='text'></input>
-                <input placeholder="data" type='data'></input>
-                <input placeholder="iduser" type='number'></input>
+            <form onSubmit={(e) => submit(e)}>
+                <input onChange={(e) => handle(e)} id="name" value={data.name} placeholder="name" type='text'></input>
+                <input onChange={(e) => handle(e)} id="data" value={data.date} placeholder="date" type='date'></input>
+                <input onChange={(e) => handle(e)} id="iduser" value={data.iduser} placeholder="iduser" type='number'></input>
+                <button>Submit</button>
             </form>
-        </div>
+        </div >
     )
 }
